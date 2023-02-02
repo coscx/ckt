897b27631ad2a5991366686ed48d001ca52fe117
wan <710666260@qq.com>
2023-02-02 22:35:33 +0800

    up to 3.7

--- lib/common/widgets/dy_behavior_null.dart
+++ lib/common/widgets/dy_behavior_null.dart
@@ -1,14 +1,10 @@
-import 'dart:io';
 import 'package:flutter/material.dart';
 
 class DyBehaviorNull extends ScrollBehavior {
   @override
-  Widget buildViewportChrome(BuildContext context, Widget child, AxisDirection axisDirection) {
-    if (Platform.isAndroid || Platform.isFuchsia) {
-      return child;
-    } else {
-      return child;
-    }
+  Widget buildOverscrollIndicator(
+      BuildContext context, Widget child, ScrollableDetails details) {
+    return child;
   }
 }
 
